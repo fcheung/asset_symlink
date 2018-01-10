588b7807c8dca2338179f9c7031e28247ce4b2a6
Frederick Cheung <frederick.cheung@gmail.com>
2018-01-10 14:53:39 +0000

    set assets.compile to false in spec - this is the more common case in production

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -19,6 +19,7 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.compile = false
     config.assets.precompile += ['3rdparty/lib.js']
     config.asset_symlink = ['application.js', {'3rdparty/lib.js' => '3rdparty/all.js'}]
   end
